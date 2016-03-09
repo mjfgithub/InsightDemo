7e702326698768eb026dfc98863cb6385e62fbcd
mjf <mjfschool@yahoo.com>
2016-03-08 20:42:35 -0500

    add insight reference

--- InsightWeb/InsightWeb/Controllers/HomeController.cs
+++ InsightWeb/InsightWeb/Controllers/HomeController.cs
@@ -8,11 +8,23 @@ namespace InsightWeb.Controllers
 {
     public class HomeController : Controller
     {
+
+       
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
-            return View();
+            var movies = new List<object>();
+
+            movies.Add(new { Title = "Ghostbusters", Genre = "Comedy", Year = 1984 });
+            movies.Add(new { Title = "Gone with Wind", Genre = "Drama", Year = 1939 });
+            movies.Add(new { Title = "Star Wars", Genre = "Science Fiction", Year = 1977 });
+
+            return Json(movies, JsonRequestBehavior.AllowGet);
+
+
+            //return View();
         }
     }
 }
